25bb438252aaa388858a5f405d68bf05671966cb
ddvv <529531385@qq.com>
2022-08-19 17:23:06 +0800

    first commit

---
+++ nextbv2/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    :   2022/08/16 21:53:25
+# @Author  :   ddvv
+# @公众号   :   NextB
+# @File    :   __init__.py
+# @Software:   Visual Studio Code
+# @Desc    :   None
---
+++ setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# @Time    :   2022/08/16 21:48:13
+# @Author  :   ddvv
+# @公众号   :   NextB
+# @File    :   setup.py
+# @Software:   Visual Studio Code
+# @Desc    :   None
+
+
+from setuptools import setup, find_packages
+
+depends = []
+
+setup(
+    name="nextbv2",
+    version="2.0.0",
+    packages=find_packages(exclude=[]),
+
+    zip_safe=False,
+
+    entry_points={
+        "console_scripts": [
+        ],
+    },
+
+    install_requires=depends,
+
+    dependency_links=[
+    ],
+
+    include_package_data=True,
+
+    license="ddvv",
+
+    author="ddvv",
+    author_email="dadavivi512@gmail.com",
+    description="nextb2.0",
+)
