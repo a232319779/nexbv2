88bf4d637c4698b90a9669e86130d2a6668d9868
ddvv <529531385@qq.com>
2022-08-22 13:17:01 +0800

    [ADD]增加币安API请求库

---
+++ nextbv2/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2022/08/22 10:02:50
+# @Author   : ddvv
+# @Site     : https://ddvvmmzz.github.io
+# @File     : main.py
+# @Software : Visual Studio Code
+# @WeChat   : NextB
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_nextbv2.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2022/08/22 10:57:55
+# @Author   : ddvv
+# @Site     : https://ddvvmmzz.github.io
+# @File     : test_nextbv2.py
+# @Software : Visual Studio Code
+# @WeChat   : NextB
+
+import pytest
+import json
+from nextbv2.configs.binance_config import BINANCE_CONFIG
+from nextbv2.libs.binance.apis import NextBBiance
+
+class TestNextBV2():
+    def test_binance(self):
+        api_key = BINANCE_CONFIG.get("api_key")
+        api_secret = BINANCE_CONFIG.get("api_secret")
+        proxies = BINANCE_CONFIG.get("proxies")
+        nb = NextBBiance(api_key, api_secret, proxies)
+        balance = nb.get_asset_balance()
+        price = nb.get_symbol_ticker("BNBUSDT")
+        assert 'asset' in balance.keys()
+        assert 'price' in price.keys()
+        print(json.dumps(balance, indent=4))
+        print(json.dumps(price, indent=4))
