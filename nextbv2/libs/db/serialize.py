9dd7b5c5ea186c0d10c445ca4e88e739ae984af7
ddvv <529531385@qq.com>
2023-02-01 16:47:15 +0800

    [UPDATE]优化币安查询数据存储与更新逻辑

--- nextbv2/libs/db/serialize.py
+++ nextbv2/libs/db/serialize.py
@@ -14,8 +14,9 @@ __doc__ = """
 
 import os
 import pickle
-import datetime
 from nextbv2.libs.logs.logs import *
+from nextbv2.libs.common.nextb_time import timestamp_to_time, get_now_timestamp
+from nextbv2.libs.common.constant import ONE_HOUR_TIMESTAMP
 
 
 class NextBSerialize(object):
@@ -34,25 +35,22 @@ class NextBSerialize(object):
         加载成功返回True，失败返回False
         数据格式如：
         {
-            "BNBUSDT": {
-                "update_time": "2020-08-22 10:00:00",
-                "data": [
-                    [
-                        1499040000000,      # Open time
-                        "0.01634790",       # Open
-                        "0.80000000",       # High
-                        "0.01575800",       # Low
-                        "0.01577100",       # Close
-                        "148976.11427815",  # Volume
-                        1499644799999,      # Close time
-                        "2434.19055334",    # Quote asset volume
-                        308,                # Number of trades
-                        "1756.87402397",    # Taker buy base asset volume
-                        "28.46694368",      # Taker buy quote asset volume
-                        "17928899.62484339" # Can be ignored
-                    ]
+            "BNBUSDT": [
+                [
+                    1499040000000,      # Open time
+                    "0.01634790",       # Open
+                    "0.80000000",       # High
+                    "0.01575800",       # Low
+                    "0.01577100",       # Close
+                    "148976.11427815",  # Volume
+                    1499644799999,      # Close time
+                    "2434.19055334",    # Quote asset volume
+                    308,                # Number of trades
+                    "1756.87402397",    # Taker buy base asset volume
+                    "28.46694368",      # Taker buy quote asset volume
+                    "17928899.62484339" # Can be ignored
                 ]
-            }
+            ]
         }
         """
         try:
@@ -93,54 +91,50 @@ class NextBSerialize(object):
         更新指定币种的K线数据
         数据格式见self.load_datas()
         """
-        # 只更新到上一个整点
-        datas = datas[:-1]
         if len(datas) == 0:
             error("更新序列化数据失败，失败原因：更新数据集为空")
             return False
-        # 获取上一个整点
-        now_time = datetime.datetime.now() - datetime.timedelta(minutes=60)
         # 已包含指定币种数据，则直接追加更新
         if symbol in self.datas.keys():
-            update_time_str = self.datas[symbol].get("update_time", "")
-            if update_time_str == "":
-                return False
-            update_time = datetime.datetime.strptime(
-                update_time_str, "%Y-%m-%d %H:00:00"
-            )
-            dlt_time = now_time - update_time
-            time_hour = dlt_time.days * 24 + dlt_time.seconds // 3600
-            # 只有当时间间隔与数据长度相等时才更新数据
-            if time_hour > 0 and time_hour == len(datas):
-                self.datas[symbol]["update_time"] = now_time.strftime(
-                    "%Y-%m-%d %H:00:00"
-                )
-                self.datas[symbol]["data"].extend(datas)
-                info("更新序列化数据成功，币种-{}新增{}条数据".format(symbol, len(datas)))
-            else:
+            last_dataset_timestamp = self.datas[symbol][-1][0]
+            first_data_timestamp = datas[0][0]
+            # 数据集中的最后一条数据与更新数据集数据时间间隔大于1个小时，则表示数据不连续，不进行更新
+            if first_data_timestamp - last_dataset_timestamp > ONE_HOUR_TIMESTAMP:
                 error(
-                    "更新序列化数据失败，失败原因：更新数据间隔时常与数据数量不一致或者不用更新。时间间隔为：{}，更新数据数量为：{}".format(
-                        time_hour, len(datas)
+                    "更新序列化数据失败，失败原因：更新时间不连续。数据集最后一条数据时间为：{}，更新数据第一条时间为：{}".format(
+                        timestamp_to_time(last_dataset_timestamp),
+                        timestamp_to_time(first_data_timestamp),
                     )
                 )
                 return False
+            # 更新数据集最后一条数据与当前时间间隔小于一小时，则删除最后一条数据
+            last_data_timestamp = datas[-1][0]
+            now_timestamp = get_now_timestamp()
+            if now_timestamp - last_data_timestamp < ONE_HOUR_TIMESTAMP:
+                del datas[-1]
+            # 默认币安返回的数据是连续的，直接插入数据集末尾
+            self.datas[symbol].extend(datas)
+            info(
+                "更新序列化数据成功，币种-{}新增{}条数据，交易数据已新至: {}".format(
+                    symbol, len(datas), timestamp_to_time(datas[-1][0])
+                )
+            )
         else:
-            self.datas[symbol] = dict()
-            self.datas[symbol]["update_time"] = now_time.strftime("%Y-%m-%d %H:00:00")
-            self.datas[symbol]["data"] = list()
-            self.datas[symbol]["data"].extend(datas)
-            info("更新序列化数据成功，新增币种类型：{}，新增数据数量：{}".format(symbol, len(datas)))
+            self.datas[symbol] = list()
+            self.datas[symbol].extend(datas)
+            info(
+                "更新序列化数据成功，新增币种：{}，新增数据数量：{}，交易数据已新至: {}".format(
+                    symbol, len(datas), timestamp_to_time(datas[-1][0])
+                )
+            )
 
         return True
 
     def get_symbol_info(self):
         """
-        返回当前数据集中包含的币种和时间信息
+        返回当前数据集中包含的币种信息
         """
-        symbol_info = dict()
-        for symobl, info in self.datas.items():
-            symbol_info[symobl] = info.get("update_time")
-
+        symbol_info = self.datas.keys()
         return symbol_info
 
     def get_datas(self):
@@ -148,3 +142,6 @@ class NextBSerialize(object):
         返回数据集
         """
         return self.datas
+
+    def get_last_data_timestamp(self, symbol):
+        return self.datas[symbol][-1][0]
